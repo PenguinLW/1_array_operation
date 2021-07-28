2f583cdb741b1c5078dcd01bae774c95aa23e353
PenguinL <diana1997525@gmail.com>
2021-07-29 00:47:36 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
+#in venv
+#pip install -r req.txt
+pip freeze > req.txt
 #git clone -o 1_array_operation https://github.com/Kovalsky95/1_array_operation.git
 #git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=3600"
+git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
 git pull --all
